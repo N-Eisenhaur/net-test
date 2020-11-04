1c4d5d32bae4ffbeba867bce3650c3923bfd37d2
codercypt <56701242+codercypt@users.noreply.github.com>
2020-11-04 18:56:29 -0400

    2nd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Yay I did it!
         </a>
       </header>
     </div>
